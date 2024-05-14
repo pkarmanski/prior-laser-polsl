ef89978f9d415e0bc6bb02375b8f51fd07ca686e
bl4szk4 <blachabartlomiej@gmail.com>
2024-05-14 17:35:08 +0200

    New component to presentation: - main.css - basic_grid.py - stage_info_grid.py - stage_management_grid.py

--- app/presentation/components/basic_grid.py
+++ app/presentation/components/basic_grid.py
@@ -8,15 +8,12 @@ class BasicGrid(QWidget):
         self.frame = QFrame()
         self.layout = QVBoxLayout()
         self.frame_layout = None
-        self.init_basic_grid()
+        self.init_grid()
 
-    def init_basic_grid(self):
+    def init_grid(self):
         self.frame.setObjectName("basic-frame")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame_layout = QVBoxLayout(self.frame)
         self.layout.addWidget(self.frame)
         self.layout.setAlignment(Qt.AlignTop)
         self.setLayout(self.layout)
-
-    def init_grid(self):
-        pass
