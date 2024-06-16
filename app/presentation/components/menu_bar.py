18087b36b9fc72d09affb5d57bda42a4b5d0e705
bl4szk4 <blachabartlomiej@gmail.com>
2024-06-16 12:29:44 +0200

    - Setting default coms for stage and laser - Updating coms

--- app/presentation/components/menu_bar.py
+++ app/presentation/components/menu_bar.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QMenuBar, QAction
-
+from app.stage_utils.utils import StageUtils
 from app.presentation.window_utils.window_utils import WindowUtils
 
 
@@ -10,12 +10,12 @@ class MenuBar:
         self.parent = main_window
 
         # actions
-        self.action_file_selection = QAction("  Plik  ", main_window)
-        self.action_notes = QAction(" Notatki ", main_window)
+        self.action_file_selection = QAction("  File  ", main_window)
+        self.action_refresh = QAction(" Update COMs ", main_window)
 
     def add_actions(self):
         self.action_file_selection.triggered.connect(lambda x: WindowUtils.open_file(self.parent))
-        self.action_notes.triggered.connect(lambda x: WindowUtils.test(self))
+        self.action_refresh.triggered.connect(self.parent.port_coms_grid.update_com_ports)
         self.file_menu.addAction(self.action_file_selection)
         self.file_menu.addSeparator()
-        self.file_menu.addAction(self.action_notes)
+        self.file_menu.addAction(self.action_refresh)
