dbdb79f853d35499f9aa527250c00116c2e07539
BigRoyalJelly <mateuszjaniak11@gmail.com>
2024-06-17 17:28:57 +0200

    Adding Icons for notifications

--- app/presentation/icons/icons.py
+++ app/presentation/icons/icons.py
@@ -5,7 +5,9 @@ from PyQt5.QtGui import QIcon
 
 class Icons(Enum):
     WINDOW_ICON = "app/resources/LaserPlaceHolder.jpg"
-
+    Success = "app/resources/success.png"
+    Error = "app/resources/error.png"
+    Choice = "app/resources/choice.png"
     def __init__(self, path):
         self.__path = path
 
